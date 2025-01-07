cbee3e04863f1ea7912c4df79f6199ff2b7411b3
Amirhossein <palizgaramirhosseyn@gmail.com>
2025-01-07 17:02:49 +0100

    jkb

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -17,9 +17,9 @@ provider "azurerm" {
   features {}
 }
 
-provider "databricks" {
-  host                = azurerm_databricks_workspace.dbworkspace.workspace_url
-  azure_client_id     = data.azurerm_key_vault_secret.apdap-devops-sc-client-id.value
-  azure_client_secret = data.azurerm_key_vault_secret.apdap-devops-sc-client-secret.value
-  azure_tenant_id     = var.tenant_id
-}
+# provider "databricks" {
+#   host                = azurerm_databricks_workspace.dbworkspace.workspace_url
+#   azure_client_id     = data.azurerm_key_vault_secret.apdap-devops-sc-client-id.value
+#   azure_client_secret = data.azurerm_key_vault_secret.apdap-devops-sc-client-secret.value
+#   azure_tenant_id     = var.tenant_id
+# }
