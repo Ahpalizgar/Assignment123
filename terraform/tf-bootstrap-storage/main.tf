c814ff9a77b78c14f609ae05bbffb0fc9ebcf293
Amirhossein <palizgaramirhosseyn@gmail.com>
2025-01-08 23:56:52 +0100

    ads

--- terraform/tf-bootstrap-storage/main.tf
+++ terraform/tf-bootstrap-storage/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "bootstrap_rg" {
 
 # Storage Account for Terraform State
 resource "azurerm_storage_account" "bootstrap_storage" {
-  name                     = "${var.bootstrap_storage_account_name}-${var.environment}"
+  name                     = "${var.bootstrap_storage_account_name}${var.environment}"
   resource_group_name      = azurerm_resource_group.bootstrap_rg.name
   location                 = azurerm_resource_group.bootstrap_rg.location
   account_tier             = "Standard"
